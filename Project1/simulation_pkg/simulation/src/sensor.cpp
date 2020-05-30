c743d78a5d0eb8dfd580dc6c8384c85855c04be7
blento <biancalento@gmail.com>
2020-05-30 10:23:46 +0200

    Update sensor.cpp

--- Project1/simulation_pkg/simulation/src/sensor.cpp
+++ Project1/simulation_pkg/simulation/src/sensor.cpp
@@ -1,13 +1,13 @@
 /**
- * \file robot node
- * \brief file for moving the robot
+ * \file sensor node
+ * \brief file for sensor behaviour
  * \author Bianca & Andrea
  * \version 0.1
- * \date 28/05/2020
+ * \date 30/05/2020
  *
  * \param[in]
     world param : x_spacing, y_spacing
-    robot param : track_gauge, wheel_radius, joint_offset, castor_arm, actuator_max_speed
+
  *
  * Subscribes to: <BR>
  *    °
@@ -16,7 +16,70 @@
  *    °
  *
  * Description
-            The aim of this file is to move the robot. So it contains all the
-            information regarding the world and the robot.
+            The aim of this file is to describe the sensors' behaviour
  *
  */
+
+//ROS
+#include "ros/ros.h"
+#include "simulation/sensor.h"
+#include "simulation_messages/SensorStatus.h"
+
+// Sensor parameters
+double x1, y1;
+nh_loc.param("x1_pos", x1, 0.2) ;
+nh_loc.param("y1_pos", y1, 0.2) ;
+
+double x2, y2;
+nh_loc.param("x2_pos", x2, 0.2) ;
+nh_loc.param("y2_pos", y2, 0.2) ;
+
+
+// Vector of sensors
+std::vector<Sensor> robotSensor;
+
+// Inizialize the vector
+robotSensor.push_back(sens1(x1_pos, y1_pos));
+robotSensor.push_back(sens2(x2_pos, y2_pos));
+
+
+// Service Callback
+bool ServiceCallback(simulation_messages::SensorStatus::Request &req,
+					simulation_messages::SensorStatus::Response &res){
+		
+		for(const auto& sens : robotSensor){
+			sens.UpdateTrasform();
+			sens.CheckSatus();
+		}
+}
+
+int main (int argc, char** argv)
+{
+
+	  // ROS Initialization
+    ros::init(argc, argv, "sensor");
+
+    //  Required NodeHandles
+    ros::NodeHandle nh_loc("~"), nh_glob;
+
+    //  Global world parameters
+    double xSpacing, ySpacing;                    // [m]
+    nh_glob.param("x_spacing", xSpacing, 0.2) ;
+    nh_glob.param("y_spacing", ySpacing, 0.2) ;
+
+  	// Declare your node's subscriptions and service clients
+  	ros::ServiceServer SensorService = nh_glob.advertiseService("sensor_status", ServiceCallback);
+  	
+  	
+
+  	ros::Rate rate(100);
+    while (ros::ok()){
+        ros::spinOnce();
+
+        
+        rate.sleep();
+    }
+
+
+    
+}
