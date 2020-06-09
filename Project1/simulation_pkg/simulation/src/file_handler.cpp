14b92e8fa9b0f2028766245a7eecb12bf82b708a
aGotelli <gotelliandrea@gmail.com>
2020-06-09 10:07:51 +0200

    correct inconsistency of non returning value in non void function

--- Project1/simulation_pkg/simulation/src/file_handler.cpp
+++ Project1/simulation_pkg/simulation/src/file_handler.cpp
@@ -14,22 +14,22 @@
  *    °
  *
  * Description
-            The aim of this file is to save all the recorded data in the proper folder. Moreover, 
+            The aim of this file is to save all the recorded data in the proper folder. Moreover,
           as the architecture is versatile and some parameters can be changed, based on the specification
-          of the project, all the informations regarding this, are also saved. In this way, the details 
+          of the project, all the informations regarding this, are also saved. In this way, the details
           of the robot, of the world and of the sensor are always visible and available.
 
             This file has a build-in method to avoid to overwrite and existing file, causing to loose some
-          precious data. However, it is recommended to use a file name without spaces and that does not end 
+          precious data. However, it is recommended to use a file name without spaces and that does not end
           with a number. Even if this node will work, the name of the file will be strongly modified.
 
-            This node has also a build in method to ensure the existence of the folder where to storage the 
-          files. If the folder does not exist then it is created in the path given. However if the path is 
+            This node has also a build in method to ensure the existence of the folder where to storage the
+          files. If the folder does not exist then it is created in the path given. However if the path is
           not correct the node is shut down and simulation goes on WITHOUT SAVING ANY FILE.
 
             This node is designed to be used in the same group of the simulation. In the case where the simulation
           is not in a group the node can handle this case. However the user is encouraged to use the namespaces i.e.
-          the groups properly. This will lead to a clearer and more understandable architecture for the simulation. 
+          the groups properly. This will lead to a clearer and more understandable architecture for the simulation.
           In fact, the group allow grouping together components that work on the same task or concept.
 
  *
@@ -105,7 +105,7 @@ bool IsEqual(const boost::filesystem::directory_entry& folderFile,
 
 
 
-bool FindFile(const boost::filesystem::path& folderPath,
+void FindFile(const boost::filesystem::path& folderPath,
               boost::filesystem::path& fileName)
 {
 
