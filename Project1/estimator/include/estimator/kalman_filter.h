a94aea5796352c3e5e2157ced1c5562f70b573e9
aGotelli <gotelliandrea@gmail.com>
2020-06-10 15:32:36 +0200

    Added kalman filter class

--- Project1/estimator/include/estimator/kalman_filter.h
+++ Project1/estimator/include/estimator/kalman_filter.h
@@ -30,37 +30,107 @@
 
 
 
+class KalmanFilter {
+public:
+  KalmanFilter()=default;
 
-class Sensor;
+  //  Initialize the P matrix using the Uncertainties on the robot posture
+  const Eigen::Matrix3d Pinit() ;
 
-class SensorMeasurements {
+  //  Propagate the error
+  inline const Eigen::Matrix3d Propagation(const Eigen::Matrix3d& P,
+                                            const Eigen::Matrix3d& A,
+                                            const Eigen::MatrixXd& B )
+                                            {return A*P*A.transpose() + B*Qbeta*B.transpose() + Qalpha; }
 
+  void Estimation(Eigen::Matrix3d& P, Eigen::Vector3d& X, const Eigen::Matrix3d& C);
 
 private:
 
+  const Eigen::Matrix2d JTCinit() ;
+
+  //  Uncertainties on the robot initial posture
+  const double sigmaX     { 8.0f };
+  const double sigmaY     { 8.0f };
+  const double sigmaTheta { 3*M_PI/180 };
+
+  //  Uncertainty on the measurement
+  const double sigmaMeasurement { 0.0 };  //  Default 0.0 not a good idea
+
+  //  Inversely proportional on how accurate the model is
+  const double sigmaTuning { 0.0 };  //  Default 0.0 not a good idea
+
+  const double trackGauge {0.4} ;             //  [m]
+  const double wheelRadius {0.05 };           //  [m]
+
+  const Eigen::Matrix2d jointToCartesian { JTCinit() };
+
+  //  The covariance matrix for the wheels
+  const Eigen::Matrix2d Qwheels { Eigen::MatrixBase<Eigen::Matrix2d>::Identity()*std::pow(sigmaTuning, 2) };
+
+  //  The covariance matrix for the input
+  const Eigen::Matrix2d Qbeta { jointToCartesian*Qwheels*jointToCartesian.transpose() };
+
+  //  The covariance matrix of...
+  const Eigen::Matrix3d Qalpha { Eigen::MatrixXd::Zero(3, 3) };
 };
 
 
 
 
+const Eigen::Matrix3d KalmanFilter::Pinit()
+{
+  Eigen::Matrix3d P;
+  P <<  sigmaX,     0,        0       ,
+          0,      sigmaY,     0       ,
+          0,        0,    sigmaTheta  ;
+
+  return P;
+}
+
+const Eigen::Matrix2d KalmanFilter::JTCinit()
+{
+  Eigen::Matrix2d temp;
+  temp <<       wheelRadius/2      ,      wheelRadius/2       ,
+           wheelRadius/trackGauge  , -wheelRadius/trackGauge  ;
+  return temp;
+}
+
+void KalmanFilter::Estimation(Eigen::Matrix3d& P, Eigen::Vector3d& X, const Eigen::Matrix3d& C)
+{
+
+  Eigen::MatrixXd K = P * C.transpose() /( (C*P*C.transpose()).value() + Qgamma) ;
+  //  estimation phase
+  X = X + K*innov ;
+  P = (Eigen::MatrixBase<Eigen::Matrix3d>::Identity() - K*C) * P ;
+}
 
 
 
+// Update of matrix A and B using the current input and robot posture
+void UpdateMatrix(const Eigen::Vector3d& X, const Eigen::Vector2d input, Eigen::Matrix3d& A, Eigen::Matrix3d& B)
+{
+  //  Fill A using definition
+  A << 1, 0, -input(0)*cos(X(2)),
+       0, 1,  input(0)*sin(X(2)),
+       0, 0,           1        ;
+
+  //  Fill B using definition
+  B << cos(X(2)), 0,
+       sin(X(2)), 0,
+          0     , 1;
+
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Compute and update vector state
+void EvolutionModel(Eigen::Vector3d& X, const Eigen::Vector2d input)
+{
+  X(0) = X(0) + input(0)*cos(X(2));
+  X(1) = X(1) + input(0)*sin(X(2));
+  X(2) = X(2) + input(1);
+}
 
 
 
